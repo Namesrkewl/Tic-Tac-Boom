1b5fc88a8839f1d705f681f8066b5869b592888d
ak007777 <khadarcoleman@gmail.com>
2023-05-06 11:24:43 -0400

    6 May

---
+++ Tic Tac Boom/Assets/Scripts/Animation/MoveMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMenu : MonoBehaviour
+{
+    private bool isMoving = false;
+
+    public void MoveToHome() {
+        if (!isMoving) {
+            isMoving = true;
+            LeanTween.moveLocal(gameObject, new Vector3(0, 0, 0), 0.5f).setEaseOutElastic().setOnComplete(StopMove);
+        }
+    }
+
+    public void MoveToBattle() {
+        if (!isMoving) {
+            isMoving = true;
+            LeanTween.moveLocal(gameObject, new Vector3(2160, 0, 0), 0.5f).setEaseOutElastic().setOnComplete(StopMove);
+        }
+    }
+
+    public void MoveToTalents() {
+        if (!isMoving) {
+            isMoving = true;
+            LeanTween.moveLocal(gameObject, new Vector3(-2160, 0, 0), 0.5f).setEaseOutElastic().setOnComplete(StopMove);
+        }
+    }
+
+    public void MoveToSkins() {
+        if (!isMoving) {
+            isMoving = true;
+            LeanTween.moveLocal(gameObject, new Vector3(0, -3840, 0), 0.5f).setEaseOutElastic().setOnComplete(StopMove);
+        }
+    }
+
+    public void MoveToShop() {
+        if (!isMoving) {
+            isMoving = true;
+            LeanTween.moveLocal(gameObject, new Vector3(0, 3840, 0), 0.5f).setEaseOutElastic().setOnComplete(StopMove);
+        }
+    }
+
+    void StopMove() {
+        isMoving = false;
+    }
+}
