b1975f680572fa1e0c0d1b75c669c7f9bff44713
Khadar Coleman <khadarcoleman@gmail.com>
2023-08-10 21:12:27 -0400

    Skill Refactor

--- Tic Tac Boom/Assets/Scripts/Game Logic/SetMenu.cs
+++ Tic Tac Boom/Assets/Scripts/Game Logic/SetMenu.cs
@@ -6,18 +6,6 @@ using TMPro;
 
 public class SetMenu : MonoBehaviour
 {
-    public void SetConfirmSkillMenu() {
-        GameObject confirmSkillMenu = GameObject.Find("ConfirmSkillMenu");
-        confirmSkillMenu.transform.GetChild(1).GetChild(0).GetComponent<Image>().sprite = gameObject.GetComponent<Image>().sprite;
-        confirmSkillMenu.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>().text = gameObject.name;
-        confirmSkillMenu.transform.GetChild(1).GetChild(2).GetComponent<TextMeshProUGUI>().text = gameObject.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text;
-        if (gameObject.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text != null) {
-            confirmSkillMenu.transform.GetChild(1).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>().text = gameObject.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text;
-        } else {
-            confirmSkillMenu.transform.GetChild(1).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>().text = "USE";
-        }
-        GameObject.Find("ActiveSkill").transform.GetChild(0).GetComponent<Image>().sprite = gameObject.GetComponent<Image>().sprite;
-    }
     public void SetChooseTalentMenu(GameObject skill) {
         GameObject chooseTalentMenu = GameObject.Find("ChooseTalentMenu");
         chooseTalentMenu.transform.GetChild(1).GetChild(0).GetComponent<Image>().sprite = gameObject.transform.GetChild(0).GetComponent<Image>().sprite;
